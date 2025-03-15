87d84f8b6db5d19757cbb4c2f54b5add945562f0
Vaishnavi <gounivaishnavi003@gmail.com>
2025-03-15 11:17:53 +0530

    Java prgms

---
+++ Programs/src/com/ciq/prgms/prgm1.java
@@ -0,0 +1,20 @@
+package com.ciq.prgms;
+
+public class prgm1 {
+
+
+                                              
+	public static void main(String arg[])	
+	{
+             	int n=5,fact=1;
+ 
+	        for(int i=1;i<=n;i++)
+	  	  {
+ 
+	    	   fact=fact*i;
+ 	 	  }
+ 
+ 	        System.out.println("factoral="+fact);
+	}
+}
+
